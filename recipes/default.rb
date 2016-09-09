159331baa44d9bc4a2df9289b6d037a87dce3bf0
Nick James <nick.james@eaglegenomics.com>
2016-09-09 10:48:12 +0100

    fixed tkinter error, changed to poise python cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,17 +6,19 @@
 
 include_recipe 'apt'
 include_recipe 'build-essential'
-include_recipe 'python'
 
 # ubuntu package dependencies for base qiime
-package %w(pkg-config libpng-dev libjpeg8-dev libfreetype6-dev) do
+# now need python-tk to prevent import _tkinter error.
+# See eg. http://stackoverflow.com/questions/26702119/installing-tkinter-on-ubuntu-14-04
+package %w(pkg-config libpng-dev libjpeg8-dev libfreetype6-dev python-tk) do
   action :install
 end
 
-python_pip 'numpy' do
-end
+python_runtime '2'
 
-python_pip 'qiime' do
+python_package 'numpy'
+
+python_package 'qiime' do
   version node['QIIME']['version']
 end
 
