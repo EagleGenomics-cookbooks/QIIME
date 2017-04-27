ce5054924e1bb4f221b2046429cc8315e72a8857
javi-gv94 <javier.garrayo@hotmail.com>
2017-04-27 15:59:42 +0100

    rspec fix

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -14,7 +14,7 @@ describe 'QIIME::default' do
     end
 
     it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+      expect { :chef_run }.to_not raise_error
     end
   end
 end
