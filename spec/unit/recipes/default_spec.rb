bf333c533ab6f5fd0f0ad19243a60d3a84217434
Nick James <nick.james@eaglegenomics.com>
2019-03-08 11:16:43 +0000

    updating to template

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,20 +1,22 @@
 #
-# Cookbook Name:: QIIME
+# Cookbook:: QIIME
 # Spec:: default
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright:: 2019, The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
 describe 'QIIME::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When all attributes are default, on Ubuntu 18.04' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '18.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
-      expect { :chef_run }.to_not raise_error
+      expect { chef_run }.to_not raise_error
     end
   end
 end
