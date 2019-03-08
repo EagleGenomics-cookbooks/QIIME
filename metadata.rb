618f91c5f07867db661828157cadd827e710dac0
Nick James <nick.james@eaglegenomics.com>
2019-03-08 11:25:45 +0000

    updated to template

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
 name 'QIIME'
-maintainer 'EagleGenomics'
+maintainer 'Eagle Genomics Ltd'
 maintainer_email 'chef@eaglegenomics.com'
-license 'Apache v2.0'
+license 'Apache-2.0'
 description 'Installs/Configures QIIME'
-long_description 'Installs/Configures QIIME'
-version '0.1.3'
-
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.0.1'
+chef_version '>= 14.10'
+supports 'ubuntu', '= 18.04'
 source_url 'https://github.com/EagleGenomics-cookbooks/QIIME'
 issues_url 'https://github.com/EagleGenomics-cookbooks/QIIME/issues'
 
